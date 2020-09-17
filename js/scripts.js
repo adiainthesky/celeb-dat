a9948d3afeb2caf14b45bf63c2ee854e69606848
Adelaide Nalley <adiainthesky@gmail.com>
2020-09-16 18:33:23 -0700

    fix branching error

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,24 @@ $(document).ready(function() {
     const genre = $("select#genre").val();
     let partner 
 
-    if (gender === "Any" && genre === "Musician") {
+    if (gender === "Non-Binary" &&  genre === "Musician") {
       partner = "Sam Smith";
+      } else if (gender === "Non-Binary" && genre === "Actor") {
+      partner = "Sara Ramirez";
       } else if (gender === "Man" && genre === "Musician") {
       partner = "Ozuna";
+      } else if (gender === "Man" && genre === "Actor") {
+      partner = "Brad Pitt";
       } else if (gender === "Woman" && genre === "Musician") {
       partner = "Cardi B";
-      } else {
-      partner = "Jackie Chan"
-      }   
+      }  else if (gender === "Woman" && genre === "Actor") {
+      partner = "Catwoman";
+      } else if (gender === "Any" && genre === "Musician") {
+      partner = "Ozuna";
+      } else if (gender === "Any" && genre === "Actor") {
+      partner = "Brad Pitt";}   
   
-    $("#partner").text(match);
+    $("#partner").text(partner);
     $("#match").show(); 
 
     event.preventDefault();
